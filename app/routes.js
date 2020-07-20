e49ac5c8bf1e413422bf4e1f8084fd043122e1f0
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2020-07-20 14:14:32 +0100

    content changes commit

--- app/routes.js
+++ app/routes.js
@@ -14,13 +14,41 @@ router.get('/start', function (req, res) {
   res.render('start')
 })
 
+// Sign in
 router.get('/sign-in', function (req, res) {
-  res.render('sign-in')
+  res.render('sign-in', {
+  })
 })
 
 router.post('/sign-in', function (req, res) {
-  req.session.userEmail = req.body.email
-  res.redirect('/company-number')
+  var errors = []
+  var emailHasError = false
+  var passwordHasError = false
+
+  if (req.session.data['email'] === '') {
+    emailHasError = true
+    errors.push({
+      text: 'Enter an email address',
+      href: '#email-error'
+    })
+  }
+  if (req.session.data['password'] === '') {
+    passwordHasError = true
+    errors.push({
+      text: 'Enter a password',
+      href: '#password-error'
+    })
+  }
+
+  if (emailHasError || passwordHasError) {
+    res.render('sign-in', {
+      errorEmail: emailHasError,
+      errorPassword: passwordHasError,
+      errorList: errors
+    })
+  } else {
+    res.redirect('/company-number')
+  }
 })
 
 router.get('/objecting-entity-name', function (req, res) {
